00740a34ee76c0ebb9bcf5c05110c3597410358b
jie zhang <>
2021-03-27 00:33:15 +0800

    springcloud 配置中心

---
+++ console/src/main/java/jiezhang/console/controller/SchedulingController.java
@@ -0,0 +1,11 @@
+package jiezhang.console.controller;
+
+import jiezhang.base.controller.BaseController;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SchedulingController
+//        extends BaseController
+
+{
+}
---
+++ freemaker/src/main/java/jiezhang/tag/web/Slider.java
@@ -0,0 +1,16 @@
+package jiezhang.tag.web;
+
+import jiezhang.tag.base.AbstractWebUiTag;
+
+/**
+ * 滑块
+ *
+ * @author ZhangJie
+ * @date 2:52 下午 2020/6/8
+ */
+public class Slider extends AbstractWebUiTag {
+    @Override
+    public String getStartTemplate() {
+        return null;
+    }
+}
